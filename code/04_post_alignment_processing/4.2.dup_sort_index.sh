2bd6e754c2c3ca6291bead51d0505c19a1d27414
Shengli Lab <76990733+lishenglilab@users.noreply.github.com>
2025-07-14 17:08:47 +0800

    Update and rename code/4.2.dup_sort_index.sh to code/04_post_alignment_processing/4.2.dup_sort_index.sh

--- code/04_post_alignment_processing/4.2.dup_sort_index.sh
+++ code/04_post_alignment_processing/4.2.dup_sort_index.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 
-
 BAM_DIR="/public/nanopore_cDNA/bam"
-
 BAM_FILES=($BAM_DIR/*.duplicates_marked.bam)
-
 INPUT_BAM=${BAM_FILES[$SLURM_ARRAY_TASK_ID-1]}
-
 SAMPLE_NAME=$(basename $INPUT_BAM .duplicates_marked.bam)
-
 SORTED_BAM="$BAM_DIR/${SAMPLE_NAME}.duplicates_marked.sorted.bam"
 
+# Sort the input BAM file. -@ specifies the number of additional threads.
 echo "Sorting $INPUT_BAM..."
 samtools sort -@ 1 -o $SORTED_BAM $INPUT_BAM
 
+# Create an index (.bai) for the newly sorted BAM file.
 echo "Indexing $SORTED_BAM..."
 samtools index $SORTED_BAM
 
