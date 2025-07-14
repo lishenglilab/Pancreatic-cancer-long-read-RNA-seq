3c55c79608a873607e15da431f0bc21b72145cae
Shengli Lab <76990733+lishenglilab@users.noreply.github.com>
2025-07-14 20:35:56 +0800

    Update and rename code/7.3.flair_quantify_tmp.sh to code/05_downstream_analysis/7.3.flair_quantify_tmp.sh

--- code/05_downstream_analysis/7.3.flair_quantify_tmp.sh
+++ code/05_downstream_analysis/7.3.flair_quantify_tmp.sh
@@ -6,5 +6,6 @@ isoforms="/public/nanopore_cDNA/flair/flair/all_samples_collapsed3.isoforms.fa"
 out_dir="/public/nanopore_cDNA/flair/quantify/"
 out_name=${out_dir}"flair_quantify"
 
+# Run flair quantify:
 flair quantify -r ${manifest} -i ${isoforms} --output ${out_name} --threads 72 --temp_dir ${out_dir} --sample_id_only --tpm
 
