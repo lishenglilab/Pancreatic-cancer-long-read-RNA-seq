9797fd06364ac860452f6637f5db9d450c042a6a
Shengli Lab <76990733+lishenglilab@users.noreply.github.com>
2025-07-14 20:27:23 +0800

    Update and rename code/7.0.bam2bed12.sh to code/05_downstream_analysis/7.0.bam2bed12.sh

--- code/05_downstream_analysis/7.0.bam2bed12.sh
+++ code/05_downstream_analysis/7.0.bam2bed12.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# This script converts BAM files to the BED12 format, which is required
 
-find . -name "*.dedup.sorted.filtered.sorted.bam" | parallel -j 20 "samtools index {}"
+find . -name "*.dedup.sorted.filtered.sorted.bam" | parallel -j 20 "samtools index {}"    # Use find and parallel to ensure all target BAM files have an index.
 
 find . -name "*.dedup.sorted.filtered.sorted.bam" | while read bam; do
     echo "python /public/software/flair-2.0.0/src/flair/bam2Bed12.py -i \"$bam\" --keep_supplementary > \"${bam%.bam}.bed\""
