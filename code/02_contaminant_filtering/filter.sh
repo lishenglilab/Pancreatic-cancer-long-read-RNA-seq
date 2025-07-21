fa6b5a3ff27a975c457ccc2d5c14da04b0ee8742
Shengli Lab <76990733+lishenglilab@users.noreply.github.com>
2025-07-21 11:00:58 +0800

    Create filter.sh

---
+++ code/02_contaminant_filtering/filter.sh
@@ -0,0 +1,16 @@
+
+
+#!/bin/bash
+
+
+cd /public/nanopore_cDNA/fastq/
+
+find . -type f -name "filtered_*.fastq" -print0 | \
+parallel -0 -j 20 '
+    file_dir="{//}"       
+    file_name="{/}"       
+    base_name="{/.}"      
+
+    cd "$file_dir" && \
+    NanoFilt -l 200 -q 7 "$file_name" > "${base_name}_nanofilt.fastq"
+'
